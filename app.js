63cff0c6eb6a64a580d21171c176d784f60e378c
Carlos Antonio <carlosantoniodelfino@gmail.com>
2021-12-04 00:42:20 -0300

    nova alteraçao

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var express = require('express');
 var app = express();
 
 
-app.set('vie engine','ejs');
-
-app.get('/tecnologia', function(req, res){
-	res.render("vie/secao/tecnologia");
-});
+app.set('view engine','ejs');
 
 app.get('/', function(req, res){
-	res.send("<html><body>Portal de noticias</body></html>");
+	res.render("home/index");
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+	res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+	res.render("noticias/noticias");
 });
 
 
